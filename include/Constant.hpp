3806a51d1183cc89e7d8d567cd0a068be7287db0
Ellipsis <rizkyfathur326@gmail.com>
2025-05-15 07:21:24 +0700

    ADD constant

---
+++ include/Constant.hpp
@@ -0,0 +1,11 @@
+#ifndef __CONSTANT_HPP__
+#define __CONSTANT_HPP__
+
+#include <stdio.h>
+
+const char PRIMARY_PIECE_SYMBOL = 'P';
+const char EXIT_SYMBOL = 'K';
+const char WALL_SYMBOL = '#';
+const char EMPTY_SYMBOL = '.';
+
+#endif
