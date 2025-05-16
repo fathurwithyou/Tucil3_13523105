0a734f910919abbc96fe3774e22bc350dbf23121
Ellipsis <rizkyfathur326@gmail.com>
2025-05-17 02:37:49 +0700

    ADD color symbol

--- include/Constant.hpp
+++ include/Constant.hpp
@@ -3,9 +3,14 @@
 
 #include <stdio.h>
 
-const char PRIMARY_PIECE_SYMBOL = 'P';
-const char EXIT_SYMBOL = 'K';
-const char WALL_SYMBOL = '#';
-const char EMPTY_SYMBOL = '.';
+constexpr const char PRIMARY_PIECE_SYMBOL = 'P';
+constexpr const char EXIT_SYMBOL = 'K';
+constexpr const char WALL_SYMBOL = '#';
+constexpr const char EMPTY_SYMBOL = '.';
+constexpr const char* BOLD = "\033[1m";
+constexpr const char* RESET = "\033[0m";
+constexpr const char* RED = "\033[31m";     // P
+constexpr const char* GREEN = "\033[32m";   // K
+constexpr const char* YELLOW = "\033[33m";  // moved piece
 
 #endif
